db4f7dd8b308ca1bfc7963881d333a2ac340434e
John Gentle <taoteg@gmail.com>
2016-07-20 10:15:27 -0500

    Cleaning up gitignore file to remove extrabeous files.

---
+++ src/msgdp/msgdp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+MODFLOW 96 SCENARIO GENERATION AND DATA PROCESSING
+Python Package for TACC HPC Resources
+
+
+A python module to prepare input models, generate case sceanrios, batch execute
+ and post-process output from MODFLOW 96 on TACC HPC resources.
+
+See README for usage instructions.
+"""
+
+print("Hello MSGDP")
